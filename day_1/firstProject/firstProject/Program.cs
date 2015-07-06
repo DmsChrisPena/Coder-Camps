8488c2b4a2ead3860a9a8fffda05f186003b425a
DmsChrisPena <cpena@dmshouston.com>
2015-07-06 11:41:44 -0500

    adding the file

---
+++ day_1/firstProject/firstProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Peanut Butter Man!");
+            //doSomething();
+            Console.ReadKey();
+        }
+    }
+}
